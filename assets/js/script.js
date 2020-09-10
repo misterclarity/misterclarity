65d8a20dfb790f8862a6d3d4d811ba30a7406df8
bkuermayr <45873859+bkuermayr@users.noreply.github.com>
2020-09-10 22:19:31 +0200

    Possible fix for contact scroll bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,12 +73,19 @@ jQuery(function ($) {
 		$('nav a, .page-scroll').click(function() {
 		  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
+			console.log(this.hash)
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
+			console.log(target)
+			console.log(target.offset().top-50)
+			if (target.length && this.hash !== '#contact' ) {
 			  $('html,body').animate({
 				scrollTop: target.offset().top-50
 			  }, 1000);
 			  return false;
+			}else {
+				$('html,body').animate({
+					scrollTop: 10039.9375
+				  }, 1000);
 			}
 		  }
 		});
