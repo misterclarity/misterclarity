fdd4c40084e63c505a26ece8fac3b0b43a056159
bkuermayr <45873859+bkuermayr@users.noreply.github.com>
2021-02-09 10:26:27 +0100

    add testimonial slider arrows

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,7 +118,9 @@ jQuery(function ($) {
 
 	$("#testimonials").slick({
 		infinite: true,
-		arrows: false,
+		arrows: true,
+		prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button class="slide-arrow next-arrow"><i class="fa fa-chevron-right"></i></button>',	
 		autoplay: true,
 		autoplaySpeed: 4000
 	});
