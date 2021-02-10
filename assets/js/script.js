551a99a0927893f42d2ff99900745d385df5bd7c
bkuermayr <45873859+bkuermayr@users.noreply.github.com>
2021-02-10 16:45:19 +0100

    slick lload

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,6 +118,7 @@ jQuery(function ($) {
 
 	$("#testimonials").slick({
 		infinite: true,
+		lazyLoad: 'ondemand',
 		arrows: true,
 		prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa fa-chevron-left"></i></button>',
 		nextArrow: '<button class="slide-arrow next-arrow"><i class="fa fa-chevron-right"></i></button>',	
